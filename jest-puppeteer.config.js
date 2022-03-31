884b1609224a1e678f7afbfe641c73b76b106916
swatikuchhal1 <97093295+swatikuchhal1@users.noreply.github.com>
2022-03-31 22:08:35 +0530

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     launch: {
-        headless: false,
+        headless: true,
         args: ['--no-sandbox',
         '--disable-setuid-sandbox',
         // This will write shared memory files into /tmp instead of /dev/shm,
